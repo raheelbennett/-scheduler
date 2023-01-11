771be74878961f675a9d8652b90bbca621cd4231
Raheel Bennett <raheel.bennett@telus.com>
2023-01-10 23:04:52 -0700

    add formatSpots function to properly display spots remaining so unit tests pass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,16 +3,24 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-let dayClass  = classNames(
-  "day-list__item", 
-  {"day-list__item--selected": props.selected,
-  "day-list__item--full": !props.spots
-});
+  let dayClass = classNames("day-list__item",{"day-list__item--selected": props.selected, "day-list__item--full": !props.spots});
+
+  const formatSpots = (spots) => {
+    if (!spots) {
+      return <h3 className="text--light">no spots remaining</h3>;
+    }
+    if (spots === 1) {
+      return <h3 className="text--light">1 spot remaining</h3>;
+    }
+    if (spots > 1) {
+      return <h3 className="text--light">{props.spots} spots remaining</h3>;
+    }
+  };
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h2 className="text--regular">{props.name}</h2>
+      {formatSpots(props.spots)}
     </li>
   );
 }

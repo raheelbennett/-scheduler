acddac411fbc98f04be66bc117e59bf694b2b96b
Raheel Bennett <raheel.bennett@telus.com>
2023-01-13 16:44:36 -0700

    create selector, test it. Also combine state in Aplication.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,62 +5,34 @@ import axios from "axios";
 import "components/Application.scss";
 import Appointment from "./Appointment";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
 
 
 export default function Application() {
-  const [day,setDay] = useState('Monday');
-  const [days,setDays] = useState([]);
+    const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
   
-  const appointmentsArray = Object.values(appointments).map((appointment) => {
+  const setDay = day => setState(prev => ({...prev, day}));
+  const setDays = days => setState(prev => ({...prev, days}));
+  const setAppointments = appointments => setState(prev => ({...prev, appointments}));
+  
+  useEffect(() => {
+    axios.get("/api/days").then(response => setDays(response.data));
+  }, []);
+  
+  useEffect(() => {
+    axios.get("/api/appointments").then(response => setAppointments(response.data));
+  }, []);
+  
+  const appointmentsArray = Object.values(state.appointments).map((appointment) => {
     return (
       <Appointment key={appointment.id} {...appointment} />
     );
   });
 
-  useEffect(() => {
-    axios.get("/api/days")
-    .then(response => setDays(response.data))
-  }, []);
-  
+  console.log("days", state.days);
 
   return (
     <main className="layout">
@@ -73,8 +45,8 @@ export default function Application() {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
